8a5eb43b7d52eb878da790b9d0fa955f55f86e0e
DE FRITSCH Evrard <evrard.defritsch@etu.estia.fr>
2025-07-26 14:58:04 +0200

    point d'accès wifi

--- esp32_IHM/src/main.cpp
+++ esp32_IHM/src/main.cpp
@@ -1,14 +1,38 @@
 #include <Arduino.h>
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <WiFiServer.h>
+
+const char* ssid = "ESP_Gateway";
+const char* password = "12345678";
+
+WiFiServer server(8888);  // Port TCP
 
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(115200);
+  WiFi.softAP(ssid, password);
+  IPAddress IP = WiFi.softAPIP();
+  Serial.print("Gateway IP : ");
+  Serial.println(IP);
+
+  server.begin();
 }
 
-// the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
-  delay(300);                      // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
-  delay(300);                      // wait for a second
+  WiFiClient client = server.available();
+  if (client) {
+    Serial.println("Client connecté !");
+    while (client.connected()) {
+      if (client.available()) {
+        String data = client.readStringUntil('\n');
+        Serial.print("Reçu : ");
+        Serial.println(data);
+
+        // (Optionnel) Réponse
+        // client.println("ACK: " + data);
+      }
+    }
+    client.stop();
+    Serial.println("Client déconnecté");
+  }
 }
