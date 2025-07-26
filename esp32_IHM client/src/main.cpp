9d7c3149bba93b5b44e8c6ea658116d63212574c
DE FRITSCH Evrard <evrard.defritsch@etu.estia.fr>
2025-07-26 15:42:58 +0200

    connection au serveur

--- esp32_IHM client/src/main.cpp
+++ esp32_IHM client/src/main.cpp
@@ -1,18 +1,55 @@
 #include <Arduino.h>
+#include <WiFi.h>
 
-// put function declarations here:
-int myFunction(int, int);
+const char* ssid = "ESP_Gateway";      // Nom du Wi-Fi créé par le Gateway
+const char* password = "12345678";
+
+const char* host = "192.168.4.1";      // IP fixe du SoftAP ESP32 Gateway
+const uint16_t port = 8888;
+
+WiFiClient client;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  delay(1000);
+
+  Serial.println("Connexion au WiFi...");
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("\nWiFi connecté !");
+  Serial.print("Adresse IP : ");
+  Serial.println(WiFi.localIP());
+
+  Serial.print("Connexion au serveur ");
+  Serial.print(host);
+  Serial.print(":");
+  Serial.println(port);
+
+  if (!client.connect(host, port)) {
+    Serial.println("Échec de connexion au serveur");
+    return;
+  }
+
+  Serial.println("Connecté au serveur !");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  if (client.connected()) {
+    client.println("Hello from client");
+    Serial.println("Message envoyé !");
+  } else {
+    Serial.println("Déconnecté du serveur. Tentative de reconnexion...");
+    if (client.connect(host, port)) {
+      Serial.println("Reconnecté !");
+    } else {
+      Serial.println("Échec de reconnexion");
+    }
+  }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  delay(2000);
 }
